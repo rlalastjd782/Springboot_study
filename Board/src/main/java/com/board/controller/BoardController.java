77cbb4b3f08e2b5ccd7d60e7803f9598e7e6af9e
rlalastjd782@naver.com <rlalastjd782@naver.com>
2022-07-11 14:35:41 +0900

    글쓰기 화면테스트 완료

--- Board/src/main/java/com/board/controller/BoardController.java
+++ Board/src/main/java/com/board/controller/BoardController.java
@@ -1,10 +1,14 @@
 package com.board.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.board.domain.BoardDTO;
 import com.board.service.BoardService;
 
 @Controller
@@ -13,18 +17,36 @@ public class BoardController {
 	@Autowired
 	private BoardService boardService;
 	
-	//글쓰기 화면으로 이동
+	//글쓰기 화면으로 이동, d이 컨트롤러를 요청한 화면에서 idx 라는 name을 가져와서 idx라는 이름을 쓰겠다.
 	@GetMapping(value = "/board/write.do")
-	public String openBoardWrite(Model model) {
-
-		String title = "제목";
-		String content = "내용";
-		String writer = "홍길동";
-
-		model.addAttribute("t", title);
-		model.addAttribute("c", content);
-		model.addAttribute("w", writer);
+	public String openBoardWrite(@RequestParam(value = "idx", required = false) Long idx, Model model) {
+		if (idx == null) {//idx가없으면 껍데기 dto 생성
+			model.addAttribute("board", new BoardDTO());
+		} else {
+			BoardDTO board = boardService.getBoardDetail(idx);
+			if (board == null) {//idx가있으면 해당 idx로 dto가져오기
+				return "redirect:/board/list.do"; //글쓰기 화면아닌 목록화면으로 이동
+			}
+			model.addAttribute("board", board);
+		}
 
 		return "board/write";
+	}
+	
+	@PostMapping(value = "/board/register.do")
+	public String registerBoard(final BoardDTO params) {
+		try {
+			boolean isRegistered = boardService.registerBoard(params);
+			if (isRegistered == false) {
+				// TODO => 게시글 등록에 실패하였다는 메시지를 전달
+			}
+		} catch (DataAccessException e) {
+			// TODO => 데이터베이스 처리 과정에 문제가 발생하였다는 메시지를 전달
+
+		} catch (Exception e) {
+			// TODO => 시스템에 문제가 발생하였다는 메시지를 전달
+		}
+
+		return "redirect:/board/list.do";
 	}
 }
